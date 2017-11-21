fbb72a15f12a4afadf683d1179deb7d6fcb3398f
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-11-21 14:49:10 -0600

    changed blogs array into an array of objects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,45 @@
 $(document).ready(function() {
     var blogs = [
-        [
-            "Personal site progress",
-            "November 21, 2017",
-            "I'm enjoying experimenting with Bootstrap here, but I am really ready to move to single-page applications, if, as it's been implied, that means we can refactor our structural HTML (separate out headers and footers)."
-        ],
-        [
-            "Company site redo",
-            "November 21, 2017",
-            "I'm looking forward to redoing this projects, especially if we can do it as a SPA. Not that I'm 100% sure what that means at this point."
-        ],
-        [
-            "Git shaming",
-            "November 21, 2017",
-            "I was thoroughly and rightly shamed yesterday for reviewing and merging my own pull requests."
-        ],
-        [
-            "Lorem ipsum",
-            "November 21, 2017",
-            "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
-        ],
-        [
-            "Lorem ipsum",
-            "November 21, 2017",
-            "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
-        ],
-        [
-            "Lorem ipsum",
-            "November 21, 2017",
-            "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
-        ]
+        {
+            title: "Personal site progress",
+            date: "November 21, 2017",
+            content: "I'm enjoying experimenting with Bootstrap here, but I am really ready to move to single-page applications, if, as it's been implied, that means we can refactor our structural HTML (separate out headers and footers)."
+        },
+        {
+            title: "Company site redo",
+            date: "November 21, 2017",
+            content: "I'm looking forward to redoing this projects, especially if we can do it as a SPA. Not that I'm 100% sure what that means at this point."
+        },
+        {
+            title: "Git shaming",
+            date: "November 21, 2017",
+            content: "I was thoroughly and rightly shamed yesterday for reviewing and merging my own pull requests."
+        },
+        {
+            title: "Lorem ipsum",
+            date: "November 21, 2017",
+            content: "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
+        },
+        {
+            title: "Lorem ipsum",
+            date: "November 21, 2017",
+            content: "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
+        },
+        {
+            title: "Lorem ipsum",
+            date: "November 21, 2017",
+            content: "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
+        }
     ]
     for (var i=0;i<blogs.length;i++) {
         if (i%3 == 0) {
             $('#blog-holder').append($('<div>',{class:'row'}));
         }
-        addBlog(blogs[i][0], blogs[i][1], blogs[i][2]);
+        addBlog(blogs[i]);
     }
 });
 
-function addBlog(title, date, content, location) {
+function addBlog(blog) {
     $('<div>',{class:'col'})
         .append(
             $('<article>',{
@@ -50,15 +50,15 @@ function addBlog(title, date, content, location) {
                 }).append(
                     $('<h4>',{
                         class: 'card-title'
-                    }).html(title)
+                    }).html(blog.title)
                 ).append(
                     $('<h6>', {
                         class: 'card-subtitle mb-2 text-muted'
-                    }).html(date)
+                    }).html(blog.date)
                 ).append(
                     $('<p>',{
                         class: 'card-text'
-                    }).html(content)
+                    }).html(blog.content)
                 )
             )
         )
