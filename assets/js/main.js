0596a1831c36f6ace246711d6f98066244155be2
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-01 14:01:56 -0600

    (step 5 reqs filled) wrote 3 more blog entries and put them all in blogs.json, which is loaded wtih an XHR in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,22 @@
 $(document).ready(function() {
-    var blogs = [
-        {
-            title: "Personal site progress",
-            date: "November 21, 2017",
-            content: "I'm enjoying experimenting with Bootstrap here, but I am really ready to move to single-page applications, if, as it's been implied, that means we can refactor our structural HTML (separate out headers and footers)."
-        },
-        {
-            title: "Company site redo",
-            date: "November 21, 2017",
-            content: "I'm looking forward to redoing this projects, especially if we can do it as a SPA. Not that I'm 100% sure what that means at this point."
-        },
-        {
-            title: "Git shaming",
-            date: "November 21, 2017",
-            content: "I was thoroughly and rightly shamed yesterday for reviewing and merging my own pull requests."
-        },
-        {
-            title: "Lorem ipsum",
-            date: "November 21, 2017",
-            content: "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
-        },
-        {
-            title: "Lorem ipsum",
-            date: "November 21, 2017",
-            content: "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
-        },
-        {
-            title: "Lorem ipsum",
-            date: "November 21, 2017",
-            content: "Dignissim vel platea ultrices integer mattis duis montes mauris et dis nisi? Ac pid nec. Aliquam. Porttitor? Pulvinar etiam risus, dictumst. Mattis dignissim, in mid mus, vel nascetur pulvinar dolor."
+    let blogs = [];
+
+    let blogRequest = new XMLHttpRequest();
+    blogRequest.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let response = JSON.parse(this.responseText);
+            blogs = response["blogs"];
+            console.log(blogs);
+            blogs.forEach(function(element, index) {
+                if (index%3 == 0) {
+                    $('#blog-holder').append($('<div>',{class:'row'}));
+                }
+                addBlog(blogs[index]);
+            });
         }
-    ]
-    for (var i=0;i<blogs.length;i++) {
-        if (i%3 == 0) {
-            $('#blog-holder').append($('<div>',{class:'row'}));
-        }
-        addBlog(blogs[i]);
-    }
+    };
+    blogRequest.open("GET", "assets/js/blogs.json", true);
+    blogRequest.send();
 });
 
 function addBlog(blog) {
