4732eb552c1a16932aee30d43022109ad2e1b7ae
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-05 11:34:40 -0600

    sep js into multiple files

---
+++ assets/js/events.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const activateEvents = (blogs) => {
+    // add click listeners to each blog
+    $('#blog-holder .card').click(function() {
+        let targetPostId = parseInt($(this).attr('id').substr(5));
+        let newBlog = blogs.filter(blog => blog.id == targetPostId)[0];
+        $('#blog-highlight .row').empty();
+        addBlog(newBlog, $('#blog-highlight .row'));
+    });
+
+    // add keyup listener to search field
+    $('#search-blogs').keyup(function() {
+        $('#blog-holder .card').removeClass("failed-search");
+        let term = $(this).val();
+        $.each(blogs, function(index) {
+            let blog = blogs[index];
+            if (blog.title.search(term) == -1 && blog.content.search(term) == -1) {
+                $(".card#post-"+blog.id).addClass("failed-search");
+            }
+        });
+    });
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,51 +1,6 @@
 "use strict";
 
-let blogs = [];
-
-// fetch blogs from blogs.json
-let blogRequest = new XMLHttpRequest();
-blogRequest.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        let response = JSON.parse(this.responseText);
-        blogs = response.blogs;
-        console.log(blogs);
-        // add each blog to #blog-holder
-        blogs.forEach(function(element, index) {
-            if (index%3 == 0) {
-                // add a new row for every third element
-                $('#blog-holder').append($('<div>',{class:'row'}));
-            }
-            // add each blog
-            addBlog(blogs[index], $('#blog-holder .row:last-child'));
-        });
-        // populate #blog-highlight
-        addBlog(blogs[0], $('#blog-highlight .row'));
-
-        // add click listeners to each blog
-        $('#blog-holder .card').click(function() {
-            let targetPostId = parseInt($(this).attr('id').substr(5));
-            let newBlog = blogs.filter(blog=>blog.id == targetPostId)[0];
-            $('#blog-highlight .row').empty();
-            addBlog(newBlog, $('#blog-highlight .row'));
-        });
-
-        // add keyup listener to search field
-        $('#search-blogs').keyup(function() {
-            $('#blog-holder .card').removeClass("failed-search");
-            let term = $(this).val();
-            $.each(blogs, function(index) {
-                let blog = blogs[index];
-                if (blog.title.search(term) == -1 && blog.content.search(term) == -1) {
-                    $(".card#post-"+blog.id).addClass("failed-search");
-                }
-            });
-        });
-    }
-};
-blogRequest.open("GET", "assets/js/blogs.json", true);
-blogRequest.send();
-
-function addBlog(blog, loc) {
+const addBlog = (blog, loc) => {
     $('<div>',{class:'col'})
         .append(
             $('<article>',{
@@ -70,4 +25,4 @@ function addBlog(blog, loc) {
             )
         )
     .appendTo(loc);
-}
+};
