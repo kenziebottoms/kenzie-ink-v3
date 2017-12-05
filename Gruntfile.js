971782443c532fdc4e96798aecb812fc1757fc70
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-05 10:17:26 -0600

    configged gruntfile for jshint and sass

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint: {
+            files: ['assets/js/**/*.js'],
+            options: {
+                predef: ["document", "console", "$"],
+                esnext: true,
+                globalstrict: true,
+                globals: {},
+                browserify: true,
+            },
+        },
+        sass: {
+            dist: {
+                files: {
+                    'assets/css/style.css' : 'assets/sass/style.scss'
+                },
+            },
+        },
+        watch: {
+            javascripts: {
+                files: ['assets/js/**/*.js'],
+                tasks: ['jshint'],
+            },
+            sass: {
+                files: ['assets/sass/**/*.scss'],
+                tasks: ['sass'],
+            },
+        },
+    });
+
+    require("matchdep")
+        .filterDev("grunt-*")
+        .forEach(grunt.loadNpmTasks);
+    
+    grunt.registerTask("default", ['jshint', 'sass', 'watch']);
+};
